9c3fa2433d52841db6f4d72257ebb1b3fa4745e4
Marcos Martins <marcos_36ma@hotmail.com>
2022-10-16 13:10:50 -0300

    Requisição popular movies

--- src/App.js
+++ src/App.js
@@ -2,16 +2,29 @@ import './App.css';
 import {Header} from "./components/Header";
 import {MoviesList} from "./components/MoviesList";
 import {MoviesContainer} from "./styles/MoviesContainer";
+import {MovieService} from "./services/MovieService";
+import {useEffect, useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-     <Header/>
-        <MoviesContainer>
-            <MoviesList movies={[]}></MoviesList>
-        </MoviesContainer>
-    </div>
-  );
+    const [movies, setMovies] = useState([]);
+    const fetchMovies = async () => {
+        const {data} = await MovieService.getMovies();
+        setMovies(data.results)
+        console.log(data.results);
+    }
+
+    useEffect(() => {
+        fetchMovies();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header/>
+            <MoviesContainer>
+                <MoviesList movies={movies}></MoviesList>
+            </MoviesContainer>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/services/MovieService.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API_KEY = 'ab46533eaac3514f935cf54850b2d96d';
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MovieService {
+    static getMovies(){
+        return axios(withBaseUrl('/movie/popular'));
+    }
+
+}
